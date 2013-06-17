ecc42d6f726fe07aa1a51c4260947852e42a79e1
Fernando Acedo <info@3bits.es>
2013-06-17 14:38:09 +0200

    First release

---
+++ README.md
@@ -0,0 +1,19 @@
+#Joomdlebs theme#
+
+Joomdlebs is a theme for moodle 2.5 and based in the Boostrap theme created by Bas Brands.
+
+It is intended to use with Joomdle, the amazing Joomla moodle integration extension, because some features are removed like the login.
+
+You can change the basic colors from the backend: site background, links, links hover and menu backgrond.
+
+
+
+Joomdlebs is licensed under a GPL (http://www.gnu.org/copyleft/gpl.html) .
+
+Feel free to use it as a base for your themes. Copy the files to another folder and rename them to start your own theme. 
+
+
+
+##Changelog##
+
+- 1.00 First release
---
+++ lang/en/theme_joomdlebs.php
@@ -0,0 +1,84 @@
+<?php
+// This file is part of the joomdleBS theme for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * DO NOT MODIFY THIS THEME!
+ * COPY IT FIRST, THEN RENAME THE COPY AND MODIFY IT INSTEAD.
+ *
+ * For full information about creating Moodle themes, see:
+ * http://docs.moodle.org/dev/Themes_2.0
+ *
+ * English Language strings file for joomdle theme
+ *
+ * @package   joomdleBS theme
+ * @copyright 2013 Fernando Acedo, http://3-bits.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ 
+ 
+$string['configtitle'] = 'joomdlebs';
+$string['pluginname'] = 'Joomdle Bootstrap';
+
+// Settings page
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['purgecaches'] = 'Purge caches';
+$string['notes'] =  'Joomdle Configuration page';
+$string['notesdesc'] = 'Configure your theme';
+$string['logo_url'] = 'Logo URL';
+$string['logo_urldesc'] = 'Enter the full URL of your custom logo, make sure it is of a proper size (300 x 100 px)';
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Add your extra CSS in this box, use it to customize your theme with a little css';
+
+
+$string['bodybgcolor'] = 'Body background color';
+$string['linkcolor'] = 'Link color';
+$string['linkhovercolor'] = 'Link hover color';
+$string['menubgcolor'] = 'Menu background color';
+
+$string['bodybgcolordesc'] = 'Body background color';
+$string['linkcolordesc'] = 'Link color';
+$string['linkhovercolordesc'] = 'Link hover color';
+$string['menubgcolordesc'] = 'Menu background color';
+
+
+
+// Theme info
+$string['choosereadme'] = 
+'<div class="clearfix"><div class="theme_screenshot"><h2>joomdle bootstrap</h2>
+<img src="joomdlebs/pix/screenshot.png" />
+<h3>Theme Credits</h3><p>
+This theme is based in the Bootstrap theme created by:<br />
+Bas Brands <br /><br />
+Created to use in Joomdle sites.
+
+</p>
+<h3>Theme Documentation:</h3><p> To be done </p>
+<h3>Report a bug:</h3><p>Send a email to support@3-bits.com</p></div>
+
+<div class="theme_description">
+<h2>About</h2><p>joomdlebs is a responsive theme based in moodle bootstrap theme that is focussed on Usability.
+<h2>Tweaks</h2><p>
+If you want to modify this theme, we recommend that you first duplicate it then rename it before making your changes. 
+This will prevent your customized theme from being overwritten by future Moodle upgrades, 
+and you\'ll still have the original files if you make a mess. 
+.</p>
+
+<h2>License</h2><p>This theme is licensed under the <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a>.</div></div>';
+
